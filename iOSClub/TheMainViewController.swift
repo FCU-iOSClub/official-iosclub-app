8f0eec801b56556923dd56301be9480c8fb87883
iMac01 <imac01@imac01de-imac.local>
2019-07-04 15:51:41 +0800

    update themain

--- iOSClub/TheMainViewController.swift
+++ iOSClub/TheMainViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class TheMainViewController: UIViewController {
+class TheMainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()

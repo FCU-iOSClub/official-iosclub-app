d666dec65f93bc4079270d0c1420ef461eb91e31
yozn <a757777@gmail.com>
2019-07-04 12:01:05 +0800

    add game vc

---
+++ iOSClub/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  iOSClub
+//
+//  Created by 劉祐炘 on 2019/7/4.
+//  Copyright © 2019 FCUiosclub. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

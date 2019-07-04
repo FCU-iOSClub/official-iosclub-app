66d85063264ce2cd6e2425ea84c2b795aad2f701
yozn <a757777@gmail.com>
2019-07-04 12:31:08 +0800

    add themainstoryboard

--- iOSClub/AppDelegate.swift
+++ iOSClub/AppDelegate.swift
@@ -20,9 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
-        let storyboard = UIStoryboard(name: "GameStoryboard", bundle: nil)
+        let storyboard = UIStoryboard(name: "TheMainStoryboard", bundle: nil)
         
-        let initialViewController = storyboard.instantiateViewController(withIdentifier: "GameViewController")
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "TheMainStoryboardVC")
         
         self.window?.rootViewController = initialViewController
         self.window?.makeKeyAndVisible()
---
+++ iOSClub/TheMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TheMainViewController.swift
+//  iOSClub
+//
+//  Created by 劉祐炘 on 2019/7/4.
+//  Copyright © 2019 FCUiosclub. All rights reserved.
+//
+
+import UIKit
+
+class TheMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

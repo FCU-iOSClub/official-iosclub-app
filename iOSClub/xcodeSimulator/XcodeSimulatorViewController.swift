5df4483d6bc4c6cbd32a81462070857d38c1d319
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2019-07-09 19:01:09 +0800

    Complete Xcode Simulator Label

---
+++ iOSClub/xcodeSimulator/XcodeSimulatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XcodeSimulatorViewController.swift
+//  iOSClub
+//
+//  Created by 黃威愷 on 2019/7/9.
+//  Copyright © 2019 FCUiosclub. All rights reserved.
+//
+
+import UIKit
+
+class XcodeSimulatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
